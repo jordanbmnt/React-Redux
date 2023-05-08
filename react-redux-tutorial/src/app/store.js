18a195fb0612cc10d9a94598ebaa2c278d031946
Jordan Maurice Beaumont <jordanbmnt@gmail.com>
2023-05-08 11:16:57 +0200

    added slice redicers tp the store

--- react-redux-tutorial/src/app/store.js
+++ react-redux-tutorial/src/app/store.js
@@ -1,5 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { counterSlice } from "../features/counter/counterSlice.js";
+const counterReducer = counterSlice.reducer;
 
 export default configureStore({
-  reducer: {},
-})
+  reducer: { counter: counterReducer },
+});
