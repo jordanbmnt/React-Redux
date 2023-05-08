f07ed74ffbbb824db6068afb6edb8d4b344eea90
Jordan Maurice Beaumont <jordanbmnt@gmail.com>
2023-05-08 11:10:13 +0200

    fixed errors in index.js

--- react-redux-tutorial/src/index.js
+++ react-redux-tutorial/src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import App from "./App.js";
 import { Provider } from "react-redux";
-import store from "./app/store";
+import store from "./app/store.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
